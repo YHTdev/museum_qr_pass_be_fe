3b6b8a23c5663bdf1aeae4a5be35e2356968c8a3
ArkarPhyoMichael <arkarphyo.yoonhanthar@gmail.com>
2022-06-15 17:18:57 +0630

    implement admin dashboard

--- client/pages/admin/index.jsx
+++ client/pages/admin/index.jsx
@@ -1,3 +1,6 @@
+import { LibraryIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import Iconx from "../../components/Atoms/Icons/iconx";
 import AdminLayout, {
   AdminBody,
   AdminTitle,
@@ -6,127 +9,74 @@ import AdminLayout, {
 const Dashboard = ({}) => {
   return (
     <AdminLayout>
-      <AdminTitle>Admin Dashboard</AdminTitle>
+      <AdminTitle className={`text-yellow-700`}>Admin Dashboard</AdminTitle>
       <AdminBody>
-        <div className="">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-            nulla, nam commodi magnam voluptatibus accusantium consectetur
-            cumque repellendus sit quod neque, illum dicta fugit eaque a, amet
-            excepturi! Et dolorum, reiciendis aperiam veniam optio ex deleniti
-            tempore expedita perferendis voluptate culpa incidunt aliquid iure.
-            In ad a optio nihil sapiente incidunt vel ducimus, aspernatur earum
-            vitae, commodi sit distinctio deserunt unde soluta. Nam, excepturi!
-            Alias corrupti doloribus iusto soluta nulla suscipit temporibus,
-            similique molestiae error, unde dolorem, quaerat minima. Quibusdam
-            iusto autem aliquid corporis, qui doloremque nihil esse at. Facilis
-            consectetur eum laboriosam minima possimus eos vel aliquam tempore
-            soluta quis id deleniti, reprehenderit fuga voluptas odit vero
-            cupiditate blanditiis autem nostrum ratione ipsam sint! Eos animi
-            non aliquam repellat totam, dolorem quasi labore sit doloribus
-            rerum, adipisci mollitia soluta. Minus voluptate recusandae nisi,
-            voluptatem voluptatum veniam at nihil numquam doloremque cupiditate
-            similique provident enim nostrum autem quaerat tenetur voluptatibus
-            deleniti explicabo illo minima. Aliquam officiis saepe aliquid.
-            Nobis dolorum ipsum reprehenderit numquam voluptate atque cumque
-            repudiandae, quam quos enim tempora, tenetur at quae? Tempore iusto
-            nesciunt earum amet fugiat natus, asperiores autem rerum. Fugiat
-            facere autem natus doloribus? Distinctio quo nam mollitia, nulla
-            necessitatibus amet dolorem dolor voluptatum dicta quod, sit
-            exercitationem laudantium esse tempore debitis quas quos in sunt
-            recusandae! Ab corrupti ad, dicta nihil sequi explicabo sapiente
-            quidem architecto earum maxime? Laboriosam veniam, minima, quidem
-            labore eaque, sed accusantium modi id pariatur quas necessitatibus
-            eos! Necessitatibus ea eveniet velit cumque consequatur est modi
-            aperiam laboriosam, corrupti unde? Blanditiis, distinctio
-            exercitationem, laboriosam cupiditate velit ullam corporis ex
-            ratione voluptates a sit maiores nesciunt qui laborum asperiores
-            placeat eveniet eum itaque, eius eaque. Reprehenderit doloribus ipsa
-            voluptates nihil, cupiditate veniam, autem odit illum dolores
-            quaerat magni porro nemo, dolore non? Eligendi veniam sunt aperiam
-            beatae, repellat modi ducimus cumque totam laboriosam quam nisi
-            reprehenderit deleniti dolores, nobis consequatur consequuntur unde
-            voluptatum quidem excepturi tenetur provident facere ipsam ipsa eum.
-            Beatae, nostrum soluta asperiores sint ipsa dolores enim ea in,
-            accusantium ex, vero obcaecati maxime possimus quibusdam ipsam nobis
-            qui accusamus sed atque? Quidem sit nisi, odio praesentium nemo
-            animi laboriosam sequi impedit amet iusto illo libero inventore
-            nulla ducimus, nobis laudantium eligendi doloremque dolorem, iste
-            provident. Magnam blanditiis sit, quisquam officia fugiat minus
-            repellat sunt porro aliquid nam quia voluptatem repellendus aut ab
-            dolorem id aliquam, esse, temporibus ducimus doloremque iste cumque
-            nemo suscipit! Inventore quasi aperiam est temporibus labore
-            voluptas qui voluptatibus quo tenetur architecto, in aliquam,
-            tempore repellat eius! Eveniet et error, non exercitationem atque,
-            neque vel veritatis aut nam sequi porro a. Corrupti saepe commodi
-            excepturi eum facere officiis debitis cupiditate aliquid dolor
-            pariatur enim minus ea, voluptatem vitae repellendus nesciunt iure
-            asperiores recusandae. Tempore iusto voluptates autem animi quia
-            expedita nesciunt ducimus placeat, quasi esse facilis harum tenetur
-            iure incidunt quo repudiandae temporibus necessitatibus ea pariatur
-            alias quas. Minima harum facere eum aspernatur, nulla ad saepe
-            libero omnis quia ullam, itaque aliquam recusandae facilis ab quas
-            iste quasi praesentium pariatur architecto fugiat excepturi atque
-            dolor in! Voluptatibus obcaecati consectetur sit fuga cupiditate
-            laborum quo reprehenderit? Eveniet suscipit debitis architecto?
-            Laborum quo minus, nisi minima dicta qui. Delectus dolorem ad neque
-            dolor nam corporis magnam aut consequatur obcaecati, mollitia
-            exercitationem voluptatum accusantium nulla ipsum aspernatur atque
-            numquam quisquam eaque molestias quam deserunt sequi nemo vitae!
-            Voluptatem tempore laborum, laboriosam minima corrupti quas et a
-            distinctio nihil ea labore cum error eius rem dignissimos natus
-            molestiae delectus exercitationem animi. Obcaecati laudantium
-            mollitia, molestiae doloribus minima id aperiam unde similique qui
-            esse, exercitationem ullam dolores porro dignissimos quam totam
-            blanditiis expedita ducimus voluptas consectetur debitis modi iusto!
-            Atque tempore accusantium quae dignissimos! Molestias labore odit
-            facere necessitatibus eligendi ipsa quidem eius, aliquam, sunt
-            magnam velit, incidunt modi illo. Voluptatum a illo ratione aliquid
-            aspernatur nisi cum beatae accusamus animi excepturi consequuntur
-            perspiciatis ipsam, provident veniam eligendi laboriosam nihil vero
-            totam distinctio ducimus, ut eveniet nobis iste! Quis ratione maxime
-            officiis, earum quidem neque eum, ut expedita suscipit nam optio
-            quos. Optio laudantium quo vel minima, repellat eum error atque
-            incidunt provident hic fuga! Nisi ipsa ipsam placeat provident
-            praesentium beatae unde hic obcaecati, repudiandae quo voluptates!
-            Modi sequi commodi possimus praesentium minus incidunt harum, quidem
-            assumenda obcaecati mollitia accusantium quos itaque eos blanditiis
-            unde delectus culpa eaque quam doloremque? Laborum facere ducimus
-            quas vero quidem vitae illo numquam excepturi veniam repellendus
-            necessitatibus quos velit, eveniet consequuntur quasi dolores
-            tempore perspiciatis! Ullam esse inventore, magnam repellat
-            doloremque, sed omnis explicabo odio quis dignissimos pariatur
-            dolores obcaecati fugit natus nobis at? Eius ad, reiciendis eligendi
-            tempora, adipisci eveniet doloribus error veritatis quia asperiores
-            accusamus inventore vel aliquid excepturi nam aperiam rerum dolor
-            consequuntur dolore ab. Doloribus, vel corrupti, blanditiis
-            cupiditate error incidunt voluptas, id sed dolores provident
-            reprehenderit sapiente animi officiis. Expedita atque voluptate sed
-            ipsam ut fuga suscipit cupiditate vel. Iure, expedita. Beatae,
-            possimus. Odit sapiente, explicabo eligendi aliquam ducimus
-            consectetur dolor. Nulla esse eius recusandae ullam placeat aliquid
-            expedita dolorum! Accusamus officiis cumque labore similique quo
-            hic! Consectetur ipsam quis ipsum natus, neque deleniti sequi
-            aliquid enim magnam dolore possimus reprehenderit cumque explicabo
-            adipisci incidunt corrupti illum, praesentium fugit hic facere!
-            Labore quis voluptate nisi dolores necessitatibus iste consequatur
-            fugit doloribus fugiat. Laboriosam eius impedit, nemo tempora porro
-            accusantium modi dicta corporis commodi quidem, repudiandae aliquam
-            veniam quasi ducimus fuga. Quod, reiciendis, aliquam reprehenderit,
-            soluta temporibus placeat distinctio sunt omnis adipisci illo
-            assumenda enim dicta amet repudiandae eveniet. Quia voluptate et
-            blanditiis fugiat! Fuga cumque deleniti doloribus eveniet esse
-            deserunt quam. In quod inventore quas eligendi vero a rem nobis,
-            libero dignissimos mollitia quia, perspiciatis nihil praesentium aut
-            dolor, doloribus corporis odio repudiandae? Neque minus, at adipisci
-            corporis tenetur eum rerum illum. Illo sit quidem cupiditate aperiam
-            totam asperiores molestias labore ducimus adipisci doloremque
-            dolores neque similique, architecto, quo numquam magni rem
-            perspiciatis vitae assumenda ipsam eligendi in? Odit error excepturi
-            iure harum quidem, voluptatem minus voluptatibus? Saepe, ad ratione
-            iste atque et rerum optio nobis doloremque fuga quibusdam, porro
-            excepturi dolor odio, in sed corrupti tempora ipsa.
-          </p>
+        <div className="grid grid-cols-4 gap-6 ">
+          <div className="shadow-xl rounded-3xl bg-yellow-500/20">
+            <Link href="admin/museums">
+              <a>
+                <div className="p-5">
+                  <Iconx
+                    icon={`LibraryIcon`}
+                    className={` w-10 h-10  text-yellow-700 shrink-0`}
+                  />
+                  <p className="mt-1 text-sm text-slate-600">Museum Count</p>
+                  <p className="mt-3 text-3xl tracking-wider text-yellow-700 ">
+                    <span>37</span>
+                  </p>
+                </div>
+              </a>
+            </Link>
+          </div>
+          <div className="shadow-xl rounded-3xl bg-yellow-500/20">
+            <Link href="admin/galleries">
+              <a>
+                <div className="p-5">
+                  <Iconx
+                    icon={`PhotographIcon`}
+                    className={` w-10 h-10  text-yellow-700 shrink-0`}
+                  />
+                  <p className="mt-1 text-sm text-slate-600">Gallery Count</p>
+                  <p className="mt-3 text-3xl tracking-wider text-yellow-700 ">
+                    <span>118</span>
+                  </p>
+                </div>
+              </a>
+            </Link>
+          </div>
+          <div className="shadow-xl rounded-3xl bg-yellow-500/20">
+            <Link href="/admin/visitors">
+              <a>
+                <div className="p-5">
+                  <Iconx
+                    icon={`UserIcon`}
+                    className={` w-10 h-10  text-yellow-700 shrink-0`}
+                  />
+                  <p className="mt-1 text-sm text-slate-600">Visitor Count</p>
+                  <p className="mt-3 space-x-4 text-3xl tracking-wider text-yellow-700 ">
+                    <span>37</span>
+                    <span className="text-sm">Today</span>
+                  </p>
+                </div>
+              </a>
+            </Link>
+          </div>
+          <div className="shadow-xl rounded-3xl bg-yellow-500/20">
+            <Link href="/admin">
+              <a>
+                <div className="p-5">
+                  <Iconx
+                    icon={`LibraryIcon`}
+                    className={` w-10 h-10  text-yellow-700 shrink-0`}
+                  />
+                  <p className="mt-1 text-sm text-slate-600">Museum Count</p>
+                  <p className="mt-3 text-3xl tracking-wider text-yellow-700 ">
+                    <span>37</span>
+                  </p>
+                </div>
+              </a>
+            </Link>
+          </div>
         </div>
       </AdminBody>
     </AdminLayout>
